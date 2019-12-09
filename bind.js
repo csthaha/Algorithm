86c95830fdcb0113e90edad823b049fbba6c0a84
songtao chen <songtao.chen@uisee.com>
2019-12-09 17:58:49 +0800

    js bind

---
+++ bind.js
@@ -0,0 +1,14 @@
+Function.prototype.myBind = function (ctx, ...args) {
+    return (...innerArgs) => this.call(ctx, ...args, ...innerArgs);
+};
+
+// test
+const a = {
+    name: "name of a"
+};
+function test(...msg) {
+    console.log(this.name);
+    console.log(...msg);
+}
+const t = test.myBind(a, "hello");
+t("world");
